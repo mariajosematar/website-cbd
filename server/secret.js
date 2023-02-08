88fc784e85954879da5b5c688f496f326b70882f
María José Mátar <80407624+mariajosematar@users.noreply.github.com>
2023-02-08 15:10:20 -0300

    Add files via upload

---
+++ server/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+    stripeSecret, // Stripe secret api key
+    emailUsername, // This is the mailgun username
+    emailPassword, // This is the mailgun password
+    adminName,
+    mongoUrl: 'mongodb://', // This can be public because it's just the user of the databas
+}
